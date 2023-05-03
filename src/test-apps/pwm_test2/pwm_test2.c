f69a6f8a55b86c93db34fc66e180398045a945c5
Bradley Thomas <bth74@uclive.ac.nz>
2023-05-04 10:17:36 +1200

    assigned button pins

--- src/test-apps/pwm_test2/pwm_test2.c
+++ src/test-apps/pwm_test2/pwm_test2.c
@@ -9,8 +9,10 @@
 #include "delay.h"
 #include "panic.h"
 
-#define PWM1_PIO PA1_PIO
-#define PWM2_PIO PA2_PIO
+#define PWM1_PIO PA0_PIO
+#define PWM2_PIO PA1_PIO
+#define PWM3_PIO PA2_PIO
+#define PWM4_PIO PB14_PIO
 
 #define PWM_FREQ_HZ 100e3
 
@@ -18,7 +20,7 @@ static const pwm_cfg_t pwm1_cfg =
 {
     .pio = PWM1_PIO,
     .period = PWM_PERIOD_DIVISOR (PWM_FREQ_HZ),
-    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 50),
+    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 75),
     .align = PWM_ALIGN_LEFT,
     .polarity = PWM_POLARITY_HIGH,
     .stop_state = PIO_OUTPUT_LOW
@@ -28,7 +30,27 @@ static const pwm_cfg_t pwm2_cfg =
 {
     .pio = PWM2_PIO,
     .period = PWM_PERIOD_DIVISOR (PWM_FREQ_HZ),
-    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 50),
+    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 75),
+    .align = PWM_ALIGN_LEFT,
+    .polarity = PWM_POLARITY_HIGH,
+    .stop_state = PIO_OUTPUT_LOW
+};
+
+static const pwm_cfg_t pwm3_cfg =
+{
+    .pio = PWM3_PIO,
+    .period = PWM_PERIOD_DIVISOR (PWM_FREQ_HZ),
+    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 75),
+    .align = PWM_ALIGN_LEFT,
+    .polarity = PWM_POLARITY_HIGH,
+    .stop_state = PIO_OUTPUT_LOW
+};
+
+static const pwm_cfg_t pwm4_cfg =
+{
+    .pio = PWM4_PIO,
+    .period = PWM_PERIOD_DIVISOR (PWM_FREQ_HZ),
+    .duty = PWM_DUTY_DIVISOR (PWM_FREQ_HZ, 75),
     .align = PWM_ALIGN_LEFT,
     .polarity = PWM_POLARITY_HIGH,
     .stop_state = PIO_OUTPUT_LOW
@@ -40,6 +62,8 @@ main (void)
 {
     pwm_t pwm1;
     pwm_t pwm2;
+    pwm_t pwm3;
+    pwm_t pwm4;
 
     pio_config_set (LED_STATUS_PIO, PIO_OUTPUT_HIGH);
 
@@ -51,7 +75,16 @@ main (void)
     if (! pwm2)
         panic (LED_ERROR_PIO, 2);
 
-    pwm_channels_start (pwm_channel_mask (pwm1) | pwm_channel_mask (pwm2));
+    pwm3 = pwm_init (&pwm3_cfg);
+    if (! pwm3)
+        panic (LED_ERROR_PIO, 3);
+        pwm3 = pwm_init (&pwm3_cfg);
+
+    pwm4 = pwm_init (&pwm4_cfg);
+    if (! pwm4)
+        panic (LED_ERROR_PIO, 4);
+
+    pwm_channels_start (pwm_channel_mask (pwm1) | pwm_channel_mask (pwm2) | pwm_channel_mask (pwm3) | pwm_channel_mask (pwm4));
 
     while (1)
     {
